6b2b776baf5233212e0a978baf0a31d70f78f11b
Yigit Aydin <yigitaydin@gmail.com>
2020-04-27 21:30:34 +0000

    Add User index page and gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
-    
-    def index
-    end
+
     
     def show
         @user = User.find(params[:id])
         @pieces = @user.pieces
     end
     
+    def index
+        @users = User.all
+        
+    end
     
     def new
         @user = User.new

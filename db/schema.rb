9020f4cf81928890f86ffae443eeed65fbedac84
Yigit Aydin <yigitaydin@gmail.com>
2020-04-27 09:19:44 +0000

    Adding Hash Mechanism

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_071033) do
+ActiveRecord::Schema.define(version: 2020_04_27_090942) do
 
   create_table "pieces", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_04_27_071033) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end

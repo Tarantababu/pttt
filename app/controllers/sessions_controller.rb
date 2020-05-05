05eb99f9c5f147c455c3921615e5f6f4d02c540c
Yigit Aydin <yigitaydin@gmail.com>
2020-05-05 09:04:50 +0000

    Deploy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,14 +9,14 @@ class SessionsController < ApplicationController
             flash[:notice] = "You've logged in successfully."
             redirect_to pieces_path
         else
-            flash.now[:alert] = "There is a problem"
+            flash.now[:alert] = "Wrong Pass or Username. Please check your credentials again."
             render 'new'
         end
     end
     
     def destroy
         session[:user_id] = nil
-        flash[:notice] = "Logout Successfull"
+        flash[:notice] = "You've logged out successfully"
         redirect_to root_path
     end
 end
